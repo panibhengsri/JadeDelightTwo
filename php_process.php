62610ac0f32631e9aef1d6ea4b7e60f2929a3e48
Pani Bhengsri <panipuck.bhengsri@tufts.edu>
2020-11-16 20:09:41 +0700

    add php

---
+++ php_process.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Form Result</title>
+</head>
+<body>
+    <?php
+        extract ($_POST);
+        echo "<p id='result'> </p>";
+        echo "<script type='text/javascript'>".
+            "document.getElementByID('result').innerText = showTime();".
+            "</script>";
+
+        $subject = "Your Order from Jade Delight";
+        $message = "<script type='text/javascript'>".
+            "document.getElementByID('result').innerText = showTime();".
+            "</script>";
+
+        mail($email,$subject,$message);
+    ?>
+
+</body>
+</html>
